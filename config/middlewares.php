3fdd9fa4f454e8c00aeba4c80dcb77d944315822
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2021-12-18 14:39:59 +0200

    set error middleware

--- config/middlewares.php
+++ config/middlewares.php
@@ -4,6 +4,9 @@ use Slim\App;
 
 return function (App $app) {
 
+    // TODO: debug mode.
+    $app->addErrorMiddleware(displayErrorDetails: false, logErrors: false, logErrorDetails: false);
+
     // Put your global middlewares here...
 
 //    $app->add(new \App\Http\Middleware\JsonBodyParserMiddleware());
