1dd8cad7d1f7e42e6811462fc70472241bfafe6b
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2021-12-18 12:17:22 +0200

    server start

--- server.php
+++ server.php
@@ -1,33 +1,24 @@
 <?php
 
-use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Swoole\Http\RequestCallback;
-use Swoole\Http\Server;
+use Swoole\Http\Server as HttpServer;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
-$app->addErrorMiddleware(true, false, true);
 
-$app->add(new \App\Http\Middleware\JsonBodyParserMiddleware());
+(function ($app) {
+    (require __DIR__ . '/config/middlewares.php')($app);
+})($app);
 
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('output with ' . date('Y-m-d H:i:s'));
-    return $response;
-});
+(function ($app) {
+    (require __DIR__ . '/config/routes.php')($app);
+})($app);
 
-$app->post('/post', function (Request $request, Response $response) {
-    $x = $request->getParsedBody();
-    return new JsonResponse([
-        'status' => 'ok',
-        'value' => $x,
-    ]);
-});
-
-$server = new Server('localhost', 9501);
+$server = new HttpServer('localhost', 9501);
 
 $server->on('request', RequestCallback::fromCallable(
     static fn (Request $request): Response => $app->handle($request)
