823bbd1e421428a4b0fe99393de88e7afb778f75
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2021-12-18 12:17:04 +0200

    middleware and route configs

---
+++ config/middlewares.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+use Slim\App;
+
+return function (App $app) {
+
+    // Put your global middlewares here...
+
+//    $app->add(new \App\Http\Middleware\JsonBodyParserMiddleware());
+};
