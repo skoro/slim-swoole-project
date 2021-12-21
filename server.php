edeb8680be1645c7c3003f2e55f6d376d3a060ea
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2021-12-21 23:03:28 +0200

    redone with swoole integration library

--- server.php
+++ server.php
@@ -1,10 +1,8 @@
 <?php
 
 use Dotenv\Dotenv;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use Swoole\Http\RequestCallback;
+use Slim\Swoole\ServerRequestFactory;
 use Swoole\Http\Server as HttpServer;
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -14,18 +12,20 @@ $dotenv->safeLoad();
 
 $app = AppFactory::create();
 
-(function ($app) {
-    (require __DIR__ . '/config/middlewares.php')($app);
-})($app);
-
-(function ($app) {
-    (require __DIR__ . '/config/routes.php')($app);
-})($app);
-
 $server = new HttpServer(env('SERVER_ADDR', 'localhost'), env('SERVER_PORT', 9501));
 
-$server->on('request', RequestCallback::fromCallable(
-    static fn (Request $request): Response => $app->handle($request)
-));
+$server->on('workerStart', function (HttpServer $server) use ($app) {
+
+    (function ($app) {
+        (require __DIR__ . '/config/middlewares.php')($app);
+    })($app);
+
+    (function ($app) {
+        (require __DIR__ . '/config/routes.php')($app);
+    })($app);
+
+});
+
+$server->on('request', ServerRequestFactory::createRequestCallback($app));
 
 $server->start();
