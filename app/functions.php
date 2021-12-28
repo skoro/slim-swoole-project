5611722e4229bdbae50e3e251c5b61c383580ecb
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2021-12-28 18:28:14 +0200

    protect functions from redefining

--- app/functions.php
+++ app/functions.php
@@ -3,35 +3,41 @@
 use Laminas\Diactoros\Response\JsonResponse;
 use PhpOption\Option;
 
-/**
- * JSON response wrapper.
- */
-function json(mixed $data, int $status = 200, array $headers = []): JsonResponse
-{
-    return new JsonResponse($data, $status, $headers);
-}
-
-/**
- * Get the environment value.
- */
-function env(string $param, mixed $default = null): mixed
-{
-    $value = $_ENV[$param] ?? $_SERVER[$param] ?? $default;
-    if ($value === $default) {
-        return $value;
+if (! function_exists('json')) {
+    /**
+     * JSON response wrapper.
+     */
+    function json(mixed $data, int $status = 200, array $headers = []): JsonResponse
+    {
+        return new JsonResponse($data, $status, $headers);
     }
-    return Option::fromValue($value)->map(fn ($value) => match (strtolower($value)) {
-        'false' => false,
-        'true'  => true,
-        'null'  => null,
-        default => $value,
-    })->get();
 }
 
-/**
- * Check whether DEBUG environment variable is set.
- */
-function is_debug_enabled(): bool
-{
-    return (bool) env('DEBUG', false);
+if (! function_exists('env')) {
+    /**
+     * Get the environment value.
+     */
+    function env(string $param, mixed $default = null): mixed
+    {
+        $value = $_ENV[$param] ?? $_SERVER[$param] ?? $default;
+        if ($value === $default) {
+            return $value;
+        }
+        return Option::fromValue($value)->map(fn ($value) => match (strtolower($value)) {
+            'false' => false,
+            'true'  => true,
+            'null'  => null,
+            default => $value,
+        })->get();
+    }
+}
+
+if (! function_exists('is_debug_enabled')) {
+    /**
+     * Check whether DEBUG environment variable is set.
+     */
+    function is_debug_enabled(): bool
+    {
+        return (bool) env('DEBUG', false);
+    }
 }
