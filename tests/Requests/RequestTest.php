49dfd559e580f3a9bdf46061375c58f919c5f85c
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2021-12-30 21:24:50 +0200

    demo of requests test case

---
+++ tests/Requests/RequestTest.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Requests;
+
+use Tests\TestCase;
+
+class RequestTest extends TestCase
+{
+    /** @test */
+    public function it_returns_status_ok_from_index_route()
+    {
+        $payload = $this->getJson('/');
+
+        $this->assertEquals(['status' => 'ok'], $payload);
+    }
+
+    /** @test */
+    public function requesting_non_existing_route_returns_404()
+    {
+        $response = $this->get('/not-found-page');
+
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+}
