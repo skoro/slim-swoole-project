e37fa3cbd64554e1c4d7bb4da112695e5fca7f23
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2022-01-05 21:39:32 +0200

    move function and config contents to app dir to be as much close as stock slim-skeleton

--- bin/server.php
+++ bin/server.php
@@ -6,8 +6,8 @@ use Slim\Swoole\ServerRequestFactory;
 use Swoole\Http\Server as HttpServer;
 
 define('ROOT_DIR', dirname(__DIR__));
-const CONFIG_DIR = ROOT_DIR . '/config/';
-const APP_DIR = ROOT_DIR . '/src/';
+const APP_DIR = ROOT_DIR . '/app/';
+const SRC_DIR = ROOT_DIR . '/src/';
 const VAR_DIR = ROOT_DIR . '/var/';
 
 require_once ROOT_DIR . '/vendor/autoload.php';
@@ -16,24 +16,24 @@ $dotenv = Dotenv::createImmutable(ROOT_DIR);
 $dotenv->safeLoad();
 
 AppFactory::setContainer((function () {
-    return (require CONFIG_DIR . 'container.php')();
+    return (require APP_DIR . 'container.php')();
 })());
 
 $app = AppFactory::create();
 
 $server = new HttpServer(env('SERVER_ADDR', 'localhost'), env('SERVER_PORT', 9501));
 $server->set((function () {
-    return include CONFIG_DIR . 'server.php';
+    return include APP_DIR . 'server.php';
 })());
 
 $server->on('workerStart', function (HttpServer $server) use ($app) {
 
     (function ($app) {
-        (require CONFIG_DIR . 'middlewares.php')($app);
+        (require APP_DIR . 'middlewares.php')($app);
     })($app);
 
     (function ($app) {
-        (require CONFIG_DIR . 'routes.php')($app);
+        (require APP_DIR . 'routes.php')($app);
     })($app);
 
 });
@@ -44,7 +44,7 @@ $server->on('start', function (HttpServer $server) use ($app) {
     if (is_debug_enabled()) {
         (function ($app, $server) {
             // Debug configuration is optional and can be ignored.
-            (include CONFIG_DIR . 'debug.php')($app, $server);
+            (include APP_DIR . 'debug.php')($app, $server);
         })($app, $server);
     }
     logger($app)->info('Server is started.');
