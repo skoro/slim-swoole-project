3263c1c581c08d70e19d3cfe14fb91241d8639e5
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2022-02-09 18:47:49 +0200

    class_name() global

--- app/functions.php
+++ app/functions.php
@@ -93,3 +93,14 @@ if (! function_exists('unique_id')) {
         return bin2hex(random_bytes($length));
     }
 }
+
+if (! function_exists('class_basename')) {
+    /**
+     * Get the class "basename" of the given object / class.
+     */
+    function class_basename(string|object $class): string
+    {
+        $class = is_object($class) ? get_class($class) : $class;
+        return basename(str_replace('\\', '/', $class));
+    }
+}
